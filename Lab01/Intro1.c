169577248503521c111611eca4ca775dc26758f8
Christian628 <32191147+Christian628@users.noreply.github.com>
2019-03-06 15:03:05 +0100

    Update Intro1.c

--- Lab01/Intro1.c
+++ Lab01/Intro1.c
@@ -1,1 +1,39 @@
+#include<stdio.h>  //This will stop warnings. (printf include)
+
+
+int
+main(void)
+{
+	printf("Hello" 
+	 
+	  " world\n"); // Cannot use implicit newline in the string.
+	
+	int i = 13;
+	int j = 12;
+
+	printf("%d\n",i);
+	//printf("%d %d\n",i);
+	//printf("%d%d\n",i);
+	
+	printf("%d %d\n",i,i);
+	printf("%d%d\n",i,i);
+
+	printf("%d\n", i++);
+	printf("%d\n", i);
+        printf("%d\n", ++i);
+
+	while(i < 20){
+		printf("%d\n",i);
+		++i;
+	}
+
+	int a = 1;
+	float k = 2.54;
+
+	while ( a <= 20){
+		printf("%d\t%f\n",a,a*k);
+		++a;
+	}
+	return  0;
+}
 
