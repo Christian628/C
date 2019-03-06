3d9a0574f9885a93c9553e7bb62cf072b900c0e5
Christian628 <32191147+Christian628@users.noreply.github.com>
2019-03-06 15:02:29 +0100

    Create moving_star.c

---
+++ Lab01/moving_star.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "Windows.h"
+#define WIDTH 50
+
+int 
+main(void)
+{
+	int length = 1;
+	int to_right = 1;
+	while (1) {
+		for (int i = 1; i < length; i++)
+		{
+			printf(" ");
+		}
+		printf("*");
+		for (int i = length+1; i <= WIDTH; i++)
+		{
+			printf(" ");
+		}
+		printf("\r");
+		Sleep(50);
+		
+		//fflush(stdout);
+		
+		if (to_right) {
+			length++;
+			if (length == 30) to_right = 0;
+		}
+		else {
+			length--;
+			if (length == 1) to_right = 1;
+		}
+	}
+	return 0;
+}
